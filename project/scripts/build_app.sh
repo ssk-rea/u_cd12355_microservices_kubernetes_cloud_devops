155e6ea2371b5537bc390edeb6429ef2e361193e
Shreedhar Kodate <shreedhar.kodate.vz@renesas.com>
2024-08-17 19:03:40 -0700

    update files

--- project/scripts/build_app.sh
+++ project/scripts/build_app.sh
@@ -1,21 +1,21 @@
-# # Update the local package index with the latest packages from the repositories
-# apt update
+# Update the local package index with the latest packages from the repositories
+apt update
 
-# # Install a couple of packages to successfully install postgresql server locally
-# apt install build-essential libpq-dev
+# Install a couple of packages to successfully install postgresql server locally
+apt install build-essential libpq-dev
 
-# # Update python modules to successfully build the required modules
-# pip install --upgrade pip setuptools wheel
+# Update python modules to successfully build the required modules
+pip install --upgrade pip setuptools wheel
 
-# pip install -r analytics/requirements.txt
+pip install -r analytics/requirements.txt
 
-# export DB_USERNAME=myuser
-# export DB_PASSWORD=mypassword
-# export DB_HOST=127.0.0.1
-# export DB_PORT=5433
-# export DB_NAME=mydatabase
+export DB_USERNAME=myuser
+export DB_PASSWORD=mypassword
+export DB_HOST=127.0.0.1
+export DB_PORT=5433
+export DB_NAME=mydatabase
 
-# python analytics/app.py
+python analytics/app.py
 
 export BASE_URL=127.0.0.1:5153
 curl $BASE_URL/api/reports/daily_usage
