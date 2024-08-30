792d16693db95d1629db39802b213b94aafee461
Shreedhar Kodate <shreedhar.kodate.vz@renesas.com>
2024-08-30 23:36:04 +0000

    update scripts

--- project/scripts/build_app.sh
+++ project/scripts/build_app.sh
@@ -1,23 +1,24 @@
 # Update the local package index with the latest packages from the repositories
+echo "Run apt update"
 apt update
 
 # Install a couple of packages to successfully install postgresql server locally
+echo "Install build requirements"
 apt install build-essential libpq-dev
 
 # Update python modules to successfully build the required modules
+echo "install pip requirements"
 pip install --upgrade pip setuptools wheel
 
+echo "Install app requirements"
 pip install -r analytics/requirements.txt
 
+echo "Export DB environment variables"
 export DB_USERNAME=myuser
 export DB_PASSWORD=mypassword
 export DB_HOST=127.0.0.1
 export DB_PORT=5433
 export DB_NAME=mydatabase
 
+echo "Start the application"
 python analytics/app.py
-
-export BASE_URL=127.0.0.1:5153
-curl $BASE_URL/api/reports/daily_usage
-
-curl $BASE_URL/api/reports/user_visits
---
+++ project/scripts/verify_app_curl.sh
@@ -0,0 +1,22 @@
+
+
+printf "\nExport app URL\n"
+export BASE_URL=127.0.0.1:5154
+printf $BASE_URL
+
+printf "\nRun health_check curl"
+curl $BASE_URL/health_check
+printf "\n"
+
+printf "\nRun readiness_check curl\n"
+curl $BASE_URL/readiness_check
+printf "\n"
+
+
+# printf "\nRun daily_usage curl"
+# curl $BASE_URL/api/reports/daily_usage
+printf "\n"
+
+# printf "\nVerify DB user_visits"
+# curl $BASE_URL/api/reports/user_visits
+printf "\n"
